f1cde86895457165c4f9ff6f7f0be3b44aea4ef9
huchangfa123 <hcf1095246249@qq.com>
2018-04-27 21:27:10 +0800

    admin basic ui fixed

---
+++ crmadmin/src/views/orderBlock/orderMessagePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="contain">
+  </div>
+</template>
+<style lang='less' scoped>
+  .contain {
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+  }
+</style>
--- crmadmin/src/views/userBlock/userMessagePage.vue
+++ crmadmin/src/views/userBlock/userMessagePage.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="contain">
+    <el-table
+      :data="tableData"
+      style="width: 500px"
+    >
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="level"
+        label="等级">
+      </el-table-column>
+      <el-table-column
+        prop="createdAt"
+        label="注册时间">
+      </el-table-column>
+      <el-table-column
+        prop="profit"
+        label="个人盈利">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      tableData: [{
+        name: '王小虎',
+        level: '黄金等级',
+        createdAt: '2018-2-21',
+        profit: '2131'
+      }]
+    }
+  }
+}
+</script>
 <style lang='less' scoped>
   .contain {
     width: 100%;
     height: 100%;
     padding: 10px;
+    display: flex;
+    justify-content: center;
   }
 </style>
