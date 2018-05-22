372cf39f861cafa83340bab9e64997342c4921fe
huchangfa123 <hcf1095246249@qq.com>
2018-05-22 23:54:53 +0800

    证书修改

--- crmh5/src/views/userBlock/credentialsPage.vue
+++ crmh5/src/views/userBlock/credentialsPage.vue
@@ -9,6 +9,12 @@
       <span class="realName">{{userData.realName}}</span>
       <span class="phoneNumber">{{userData.phoneNumber}}</span>
       <span class="agent">{{userData.agent}}</span>
+      <span class="fromYear">{{new Date(userData.createAt).getFullYear()}}</span>
+      <span class="fromMonth">{{new Date(userData.createAt).getMonth() + 1}}</span>
+      <span class="fromDay">{{new Date(userData.createAt).getDate()}}</span>
+      <span class="toYear">{{new Date(userData.expiredAt).getFullYear()}}</span>
+      <span class="toMonth">{{new Date(userData.expiredAt).getMonth() + 1}}</span>
+      <span class="toDay">{{new Date(userData.expiredAt).getDate()}}</span>
       <span class="authorizationCode">{{userData.authorizationCode}}</span>
     </div>
   </div>
@@ -53,6 +59,36 @@ export default {
       position: absolute;
       top: 46%;
       left: 32%;
+    }
+    .fromYear {
+      position: absolute;
+      bottom: 30%;
+      left: 20%;
+    }
+    .fromMonth {
+      position: absolute;
+      bottom: 30%;
+      left: 34%;
+    }
+    .fromDay {
+      position: absolute;
+      bottom: 30%;
+      left: 39%;
+    }
+    .toYear {
+      position: absolute;
+      bottom: 30%;
+      left: 47%;
+    }
+    .toMonth {
+      position: absolute;
+      bottom: 30%;
+      left: 59%;
+    }
+    .toDay {
+      position: absolute;
+      bottom: 30%;
+      left: 64%;
     }
     .phoneNumber {
       position: absolute;
